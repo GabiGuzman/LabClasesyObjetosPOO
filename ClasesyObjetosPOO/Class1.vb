ef5593591a175e281422f1aa02cf150041b8e222
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 11:35:27 -0600

    Agregar archivos de proyecto.

---
+++ ClasesyObjetosPOO/Class1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Class1
+    'PROPIEDADES'
+    Public nombre As String
+    Public genero As String
+    Public altura As String
+    Public ciudad As String
+
+    'CONSTRUCTOR'
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(nombre As String, raza As String, altura As String)
+        Me.nombre = nombre
+        Me.genero = genero
+        Me.altura = altura
+        Me.ciudad = ciudad
+    End Sub
+
+    'METODO'
+    Public Function comer(Carne As String) As String
+        Return Me.nombre & "genero " & Me.genero & "y mide " & Me.altura & "es de " & Me.ciudad & "comerá " & Carne
+    End Function
+
+    Public Function dormir(Ocho As String)
+        Return Me.nombre & "genero " & Me.genero & "y mide " & Me.altura & "es de " & Me.ciudad & "Duerme " & Ocho
+    End Function
+
+End Class
---
+++ ClasesyObjetosPOO/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim persona1 As Class1 = New Class1()
+        persona1.nombre = "Amelia "
+        persona1.genero = "Femenino "
+        persona1.altura = "1.64 mt "
+        persona1.ciudad = "Mazatenango"
+        TextBox1.Text = persona1.comer("Pollo asado")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim persona2 As Class1 = New Class1()
+        persona2.nombre = "Waldir "
+        persona2.genero = "masculino "
+        persona2.altura = "1.70 mt "
+        persona2.ciudad = "Guatemala "
+        TextBox1.Text = persona2.dormir("Ocho horas ")
+    End Sub
+End Class
